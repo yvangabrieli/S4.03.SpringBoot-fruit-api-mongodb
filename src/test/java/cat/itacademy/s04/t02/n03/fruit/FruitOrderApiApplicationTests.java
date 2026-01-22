d8fd6c6d834e7078d23186871e947be4503a49e4
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2026-01-22 21:35:54 +0100

    feat:Initial Spring Boot MongoDB project setup

---
+++ src/test/java/cat/itacademy/s04/t02/n03/fruit/FruitOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s04.t02.n03.fruit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
